0c60483dc78e7b98b077ed196c43336ace1458b1
Yaroslav Sydoryk <slaviksydoryk@gmail.com>
2017-04-08 13:12:13 +0300

    fixed startd day

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
-createCalendar('root', 2016, 12)
+createCalendar('root', 2016, 1);
 
 function createCalendar(id, year, month) {
+    var calendar = document.createElement('div');
 
-    var calendarContainer = document.createElement('div');
+    calendar.className = 'calendarContainer';
+    calendar.appendChild(renderHeader(year, month));
+    calendar.appendChild(renderTable(year, month));
 
-    calendarContainer.classList.add('calendarContainer');
-    calendarContainer.appendChild(renderHeader(year, month));
-    calendarContainer.appendChild(renderTable(year, month));
-
-    var toPage = document.getElementById(id).appendChild(calendarContainer);
+    document.getElementById(id).appendChild(calendar);
 }
 
 function renderHeader(year, month) {
     var header = document.createElement('header'),
         monthElem = document.createElement('span');
     yearElem = document.createElement('span');
+    getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' });
 
-    header.classList.add('header');
-    var getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' });
+    header.className = 'header';
+    monthElem.className = 'header__month';
+    yearElem.className = 'header__year';
+
     monthElem.textContent = getMonthName;
     yearElem.textContent = year;
-    monthElem.classList.add('header__month');
-    yearElem.classList.add('header__year');
 
     header.appendChild(monthElem);
     header.appendChild(yearElem);
@@ -43,7 +43,7 @@ function renderTHead() {
         tHead = document.createElement('thead');
 
     var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    row.classList.add('daysRow');
+    row.className = 'daysRow';
     days.forEach(function (item) {
         var th = document.createElement('th');
         if (item == 'Saturday' || item == 'Sunday') {
@@ -52,29 +52,28 @@ function renderTHead() {
         th.textContent = item;
         row.appendChild(th);
     });
+
     return tHead.appendChild(row);
 }
 
 function renderTBody(year, month) {
     var tBody = document.createElement('tbody'),
-        getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' });
         daysInMouth = new Date(year, month, 0).getDate(),
-        startDay = new Date(year, month - 1).getDay(),
+        startDay = new Date(year, month - 1).getDay();
         dayCount = 1;
 
-    if (startDay == 0) {
-        startDay = 7;
-    }
-    startDay--;
+    (startDay == 0) ? startDay = 6 : startDay--;
     var rowCount = Math.ceil((daysInMouth + startDay) / 7);
-
+    
     for (var i = 0; i < rowCount; i++) {
         var tr = document.createElement('tr');
 
         for (var j = 0; j < 7; j++) {
             var td = document.createElement('td');
+
             if (!(i == 0 && j < startDay) && dayCount <= daysInMouth) {
                 td.textContent = dayCount;
+                td.className = 'date';
                 dayCount++;
                 if (j == 5 || j == 6) {
                     td.style.color = '#EE6E73';
@@ -87,25 +86,26 @@ function renderTBody(year, month) {
     }
 
     // modal window on click
-    var modal = new tingle.modal({
-        footer: true,
-        stickyFooter: false,
-        closeLabel: "Close",
-        cssClass: ['custom-class-1', 'custom-class-2'],
-        beforeClose: function () {
-            return true;
-            return false;
-        }
-    });
+    var getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' }),
+        modal = new tingle.modal({
+            footer: true,
+            stickyFooter: false,
+            closeLabel: "Close",
+            cssClass: ['custom-class-1', 'custom-class-2'],
+            beforeClose: function () {
+                return true;
+                return false;
+            }
+        });
     tBody.addEventListener('click', function (e) {
         if (e.target.textContent) {
             modal.setContent('<div class="modal_container">' +
-            '<div class="modal__cal">' + 
+                '<div class="modal__cal">' +
                 '<span class="modal__ribbon"></span>' +
-                '<span class="modal__day">'+ e.target.textContent +'</span>' +
-            '</div>' +
-            '<span class="modal__month">' + getMonthName + '</span>' + 
-            '</div>');
+                '<span class="modal__day">' + e.target.textContent + '</span>' +
+                '</div>' +
+                '<span class="modal__month">' + getMonthName + '</span>' +
+                '</div>');
             modal.open();
         }
     });
