b6c6631381334f318446ce99994457bc7cb800cc
Yaroslav Sydoryk <slaviksydoryk@gmail.com>
2017-04-08 11:51:05 +0300

    click on cell added!

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 createCalendar('root', 2016, 12)
+
 function createCalendar(id, year, month) {
 
     var calendarContainer = document.createElement('div');
@@ -16,7 +17,7 @@ function renderHeader(year, month) {
     yearElem = document.createElement('span');
 
     header.classList.add('header');
-    var getMonthName = new Date(year, month-1).toLocaleDateString('en-us', {month: 'long'});
+    var getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' });
     monthElem.textContent = getMonthName;
     yearElem.textContent = year;
     monthElem.classList.add('header__month');
@@ -34,7 +35,6 @@ function renderTable(year, month) {
     table.appendChild(renderTHead());
     table.appendChild(renderTBody(year, month));
 
-
     return table;
 }
 
@@ -47,7 +47,7 @@ function renderTHead() {
     days.forEach(function (item) {
         var th = document.createElement('th');
         if (item == 'Saturday' || item == 'Sunday') {
-            th.style.color = 'red';
+            th.style.color = '#EE6E73';
         }
         th.textContent = item;
         row.appendChild(th);
@@ -57,32 +57,57 @@ function renderTHead() {
 
 function renderTBody(year, month) {
     var tBody = document.createElement('tbody'),
+        getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' });
         daysInMouth = new Date(year, month, 0).getDate(),
         startDay = new Date(year, month - 1).getDay(),
         dayCount = 1;
-        if(startDay == 0) {
-            startDay = 7;
-        }
-        startDay--;
-        var rowCount = Math.ceil((daysInMouth + startDay) / 7);
-   
+
+    if (startDay == 0) {
+        startDay = 7;
+    }
+    startDay--;
+    var rowCount = Math.ceil((daysInMouth + startDay) / 7);
+
     for (var i = 0; i < rowCount; i++) {
         var tr = document.createElement('tr');
 
-        for(var j = 0; j < 7; j++){
+        for (var j = 0; j < 7; j++) {
             var td = document.createElement('td');
-            if(!(i == 0 && j < startDay) && dayCount <= daysInMouth){
+            if (!(i == 0 && j < startDay) && dayCount <= daysInMouth) {
                 td.textContent = dayCount;
                 dayCount++;
-                if(j == 5 || j == 6) {
-                    td.style.color = 'red';
+                if (j == 5 || j == 6) {
+                    td.style.color = '#EE6E73';
                 }
             }
             tr.appendChild(td);
         }
-        
+
         tBody.appendChild(tr);
     }
 
+    // modal window on click
+    var modal = new tingle.modal({
+        footer: true,
+        stickyFooter: false,
+        closeLabel: "Close",
+        cssClass: ['custom-class-1', 'custom-class-2'],
+        beforeClose: function () {
+            return true;
+            return false;
+        }
+    });
+    tBody.addEventListener('click', function (e) {
+        if (e.target.textContent) {
+            modal.setContent('<div class="modal_container">' +
+            '<div class="modal__cal">' + 
+                '<span class="modal__ribbon"></span>' +
+                '<span class="modal__day">'+ e.target.textContent +'</span>' +
+            '</div>' +
+            '<span class="modal__month">' + getMonthName + '</span>' + 
+            '</div>');
+            modal.open();
+        }
+    });
     return tBody;
 }
