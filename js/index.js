1c55c19906d611e23ea7ab1907f7cf9b888c372e
Yaroslav Sydoryk <slaviksydoryk@gmail.com>
2017-04-09 10:43:18 +0300

    comments added

--- js/index.js
+++ js/index.js
@@ -12,9 +12,9 @@ function createCalendar(id, year, month) {
 
 function renderHeader(year, month) {
     var header = document.createElement('header'),
-        monthElem = document.createElement('span');
-    yearElem = document.createElement('span');
-    getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' });
+        monthElem = document.createElement('span'),
+        yearElem = document.createElement('span'),
+        getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' }); // convert number month to string
 
     header.className = 'header';
     monthElem.className = 'header__month';
@@ -40,17 +40,17 @@ function renderTable(year, month) {
 
 function renderTHead() {
     var row = document.createElement('tr'),
-        tHead = document.createElement('thead');
+        tHead = document.createElement('thead'),
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     row.className = 'daysRow';
-    days.forEach(function (item) {
+    days.forEach(function (item) { // each day add to row
         var th = document.createElement('th');
-        if (item == 'Saturday' || item == 'Sunday') {
+        if (item == 'Saturday' || item == 'Sunday') { // if weekend day add color red
             th.style.color = '#EE6E73';
         }
         th.textContent = item;
-        row.appendChild(th);
+        row.appendChild(th); 
     });
 
     return tHead.appendChild(row);
@@ -62,20 +62,20 @@ function renderTBody(year, month) {
         startDay = new Date(year, month - 1).getDay();
         dayCount = 1;
 
-    (startDay == 0) ? startDay = 6 : startDay--;
-    var rowCount = Math.ceil((daysInMouth + startDay) / 7);
-    
-    for (var i = 0; i < rowCount; i++) {
+    (startDay == 0) ? startDay = 6 : startDay--; // chack for sunday(by default sunday = 0) if sunday then make sunday equel 6 
+    // every day make decrement because we start week from monday
+    var rowCount = Math.ceil((daysInMouth + startDay) / 7); // count how many weeks
+
+    for (var i = 0; i < rowCount; i++) { 
         var tr = document.createElement('tr');
 
-        for (var j = 0; j < 7; j++) {
+        for (var j = 0; j < 7; j++) { 
             var td = document.createElement('td');
-
-            if (!(i == 0 && j < startDay) && dayCount <= daysInMouth) {
+            if (!(i == 0 && j < startDay) && dayCount <= daysInMouth) { // check for empty cells 
                 td.textContent = dayCount;
                 td.className = 'date';
-                dayCount++;
-                if (j == 5 || j == 6) {
+                dayCount++; // increment day for next iteration
+                if (j == 5 || j == 6) { // check for weekends days (make red)
                     td.style.color = '#EE6E73';
                 }
             }
@@ -86,19 +86,18 @@ function renderTBody(year, month) {
     }
 
     // modal window on click
-    var getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' }),
-        modal = new tingle.modal({
+    var getMonthName = new Date(year, month - 1).toLocaleDateString('en-us', { month: 'long' }), // convert month to string
+        modal = new tingle.modal({ // for modal used tingle script 
             footer: true,
             stickyFooter: false,
-            closeLabel: "Close",
             cssClass: ['custom-class-1', 'custom-class-2'],
             beforeClose: function () {
-                return true;
-                return false;
+                return true; // close modal
             }
         });
-    tBody.addEventListener('click', function (e) {
-        if (e.target.textContent) {
+
+    tBody.addEventListener('click', function (e) { 
+        if (e.target.textContent) { // when click on cell with number then make modal
             modal.setContent('<div class="modal_container">' +
                 '<div class="modal__cal">' +
                 '<span class="modal__ribbon"></span>' +
